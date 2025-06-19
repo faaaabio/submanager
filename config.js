0bc050876cab6d0850351a83a4511e694e58a08f
Fabio Lino <53346601+faaaabio@users.noreply.github.com>
2025-06-19 19:42:37 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Configuração do Supabase
-export async function getSupabaseConfig() {
-    return {
-        url: 'https://slnyyxutbgpbqcfzjyxi.supabase.co',
-        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbnl5eHV0YmdwYnFjZnpqeXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4MjI5NDgsImV4cCI6MjA2NDM5ODk0OH0.4C0wHwQih8NDWKVIAzoByZRe5HXQPlcvCwUgwTrqq0s'
-    }
-} 
